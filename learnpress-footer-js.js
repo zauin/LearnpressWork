6e5d50565a898bf087c8f3cac65ab9d6072ceda8
zauin <zauin09@gmail.com>
2021-05-27 10:36:08 +0900

    푸터 js 추가

---
+++ learnpress-footer-js.js
@@ -0,0 +1,10 @@
+<!-- https://tera.co.kr/wp-admin/customize.php?autofocus[section]=obfx_header_footer_scripts -->
+<script>
+setCookie('LP','{"courses-layout":"grid"}');
+
+function setCookie(key, value) {
+  var expires = new Date();
+  expires.setTime(expires.getTime() + (value * 24 * 60 * 60 * 1000));
+  document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+}
+</script>
